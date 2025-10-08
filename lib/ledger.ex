a8c9459aa41eb2f99feb53da82227aabb99e13fb
Facu <gabylope05@gmail.com>
2025-10-08 01:31:01 -0300

    chore: conecto los handler de monedas

--- lib/ledger.ex
+++ lib/ledger.ex
@@ -1,6 +1,7 @@
 defmodule Ledger do
   alias Ledger.Parser.Parser
   alias Ledger.Usuarios.HandleUsuario
+  alias Ledger.Monedas.HandleMoneda
 
   def main(args) do
     case Parser.parser_args(args) do
@@ -9,10 +10,13 @@ defmodule Ledger do
       {:ver_usuario, params} -> HandleUsuario.handle({:ver_usuario, params})
 
       {:borrar_usuario, params} -> IO.inspect({:borrar_usuario, params})
-      {:crear_moneda, params} -> IO.inspect({:crear_moneda, params})
-      {:editar_moneda, params} -> IO.inspect({:editar_moneda, params})
+
+      {:crear_moneda, params} -> HandleMoneda.handle({:crear_moneda, params})
+      {:editar_moneda, params} -> HandleMoneda.handle({:editar_moneda, params})
+      {:ver_moneda, params} -> HandleMoneda.handle({:ver_moneda, params})
+
       {:borrar_moneda, params} -> IO.inspect({:borrar_moneda, params})
-      {:ver_moneda, params} -> IO.inspect({:ver_moneda, params})
+
       {:alta_cuenta, params} -> IO.inspect({:alta_cuenta, params})
       {:realizar_transferencia, params} -> IO.inspect({:realizar_transferencia, params})
       {:realizar_swap, params} -> IO.inspect({:realizar_swap, params})
