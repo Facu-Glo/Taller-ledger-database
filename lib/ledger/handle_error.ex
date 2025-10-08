7311a2cc04f704ddac19f2fcfdf652953c98eb0d
Facu <gabylope05@gmail.com>
2025-10-08 01:30:08 -0300

    feat: agrego handler output y error  para monedas

--- lib/ledger/handle_error.ex
+++ lib/ledger/handle_error.ex
@@ -12,8 +12,42 @@ defmodule Ledger.HandleError do
 
       :invalid_age ->
         IO.inspect({:error, crear_usuario: "El usuario debe ser mayor de 18 años."})
+
+      :same_name ->
+        IO.inspect({:error, editar_usuario: "El nuevo nombre no puede ser igual al actual."})
+
       _ ->
         IO.puts("Error desconocido: #{inspect(reason)}")
+    end
+  end
+
+  def handle_monedas({:error, reason}) do
+    case reason do
+      :nombre_corto ->
+        IO.inspect({:error, crear_moneda: "El nombre de la moneda debe tener al menos 3 letras."})
+
+      :nombre_largo ->
+        IO.inspect(
+          {:error, crear_moneda: "El nombre de la moneda no puede tener más de 4 letras."}
+        )
+
+      :nombre_formato_invalido ->
+        IO.inspect({:error, crear_moneda: "El nombre de la moneda debe estar en mayúsculas."})
+
+      :divisa_existe ->
+        IO.inspect({:error, crear_moneda: "El nombre de la moneda ya se encuentra en uso."})
+
+      :precio_invalido ->
+        IO.inspect({:error, crear_moneda: "El precio de la moneda debe ser un número positivo."})
+
+      :moneda_not_found ->
+        IO.inspect({:error, editar_moneda: "No se encontró la moneda."})
+
+      :invalid_update ->
+        IO.inspect({:error, editar_moneda: "No se pudo actualizar la moneda."})
+
+      _ ->
+        IO.puts("Ocurrió un error desconocido al crear la moneda: #{inspect(reason)}")
     end
   end
 end
--- lib/ledger/handle_output.ex
+++ lib/ledger/handle_output.ex
@@ -1,16 +1,25 @@
 defmodule Ledger.HandleOutput do
   def handle({:usuario, usuario}) do
     IO.puts("""
-    ───────────────────────────────
+    ──────────────────────────────────────────────────────────────
       Usuario ID: #{usuario.id}
       Nombre: #{usuario.nombre}
       Fecha de nacimiento: #{usuario.fecha_nacimiento}
       Fecha de creación: #{usuario.fecha_creacion}
       Última edición: #{usuario.fecha_edicion}
-    ───────────────────────────────
+    ──────────────────────────────────────────────────────────────
     """)
   end
 
-  def handle({:moneda, _moneda}) do
+  def handle({:moneda, moneda}) do
+    IO.puts("""
+    ──────────────────────────────────────────────────────────────
+      Moneda ID: #{moneda.id}
+      Nombre: #{moneda.nombre}
+      Precio USD: #{moneda.precio_usd}
+      Fecha de creación: #{moneda.fecha_creacion}
+      Última edición: #{moneda.fecha_edicion}
+    ──────────────────────────────────────────────────────────────
+    """)
   end
 end
